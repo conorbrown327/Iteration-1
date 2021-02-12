71c3424466f3932af248faee78d40e590c7c31a0
Conor <brow4339@umn.edu>
2021-02-12 17:27:05 -0600

    add(lab07) deliverables

--- labs/lab07_gdb/main.cc
+++ labs/lab07_gdb/main.cc
@@ -24,7 +24,8 @@ void doInterestingThing(int yr) {
   array_len = NULL;
   d2 = Date(yr, 4, 19);
   d3 = d2.copy();
-  *array_len = 4;
+  int array_len_ = 4;
+  array_len = &array_len_;
 
   cout << "   default date is: " << d1.show() << endl;
 
@@ -41,16 +42,21 @@ void doInterestingThing(int yr) {
   cout << " ... Now print out 4 dates in reverse time order ..." << endl;
   
   // now show the dates accumulated in reverse order
-  for (int i = 0; i < *array_len; --i) {
+  for (int i = *array_len - 1; i >= 0; --i) {
     cout << "   date is: " << birthday_list[i]->show() << endl;
   }
 
   // now set the dates to January 2
-  for (int i = 0; i <= *array_len; i++) {
+  for (int i = 0; i < *array_len; i++) {
     *birthday_list[i] = Date(yr+i, 1, 2);
   }
   
   cout << "   the first date is: " << birthday_list[0]->show() << endl<< endl;
+
+  for (int i = 0; i < *array_len; i++) {
+    delete birthday_list[i];
+  }
+  delete [] birthday_list;
 }
 
 int main() {
