674e20f1bc3ec1fa206bee6ffe4b288fce6fea1c
Conor <brow4339@umn.edu>
2021-02-24 23:56:27 -0600

    Adding lab09 deliverables

--- labs/lab09_google_tests/date_unittest.cc
+++ labs/lab09_google_tests/date_unittest.cc
@@ -83,10 +83,56 @@ TEST_F(DateTest, DaysBetweenTests) {
 }
 
 TEST_F(DateTest, mutant_epoch_constructor_year) {
-  first_day = Date(0); 
-  last_day = Date(2147483647);
-  EXPECT_EQ(first_day.GetUsDate(), "01-01-1970") << "First day of class not setup properly";
-  EXPECT_EQ(last_day.GetUsDate(), "01-19-2038") << "Last day of class not setup properly";
+  first_day = Date(0);
+
+  EXPECT_EQ(first_day.GetUsDate(), "01-01-1970") << "When using the epoch constructor the min value is not correct.";
+}
+/*
+TEST_F(DateTest, mutant_constructor_no_add_1900_assess_new) {
+  first_day = Date(2000, 1, 1);
+
+  EXPECT_EQ(first_day.GetUsDate(), "01-01-2000") << "The date Jan 01, 2000 is incorrectly initialized.";
+}
+*/
+TEST_F(DateTest, mutant_op_minus_arithmetic) {
+  first_day = Date(1970, 2, 1);
+  last_day = first_day - 1;
+
+  EXPECT_EQ(last_day.GetUsDate(), "01-01-1970") << "The date Jan 02, 1970 - 1 day did not compute correctly.";
+}
+
+TEST_F(DateTest, mutant_days_between_no_leap ) {
+  first_day = Date(2016, 1, 1);
+  last_day = Date(2016, 3, 1);
+
+  EXPECT_EQ(first_day.DaysBetween(last_day), 60) << "Did not account for leap year or other days between calc error, should be 60 days.";
+}
+
+TEST_F(DateTest, mutant_get_date_separator_omit ) {
+  first_day = Date(1970, 1, 1);
+
+  EXPECT_EQ(first_day.GetDate(), "1970-01-01") << "The date is formatted incorretly in GetDate().";
+}
+
+TEST_F(DateTest, mutant_get_us_date_wrong_format ) {
+  first_day = Date(2018, 12, 11);
+
+  EXPECT_EQ(first_day.GetUsDate(), "12-11-2018") << "The date is formatted incorretly in GetUsDate().";
+}
+
+/*
+TEST_F(DateTest, mutant_days_between_abs_assess ) {
+  first_day = Date(2016, 1, 1);
+  last_day = Date(2016, 3, 1);
+
+  EXPECT_GT(first_day.DaysBetween(last_day), -1) << "The date between should always be positive.";
+}
+*/
+TEST_F(DateTest, mutant_days_between_return_1_assess_new ) {
+  first_day = Date(2016, 6, 1);
+  last_day = Date(2016, 6, 15);
+
+  EXPECT_EQ(first_day.DaysBetween(last_day), 15) << "The DaysBetween function does not return the proper value.";
 }
 /**
   *
