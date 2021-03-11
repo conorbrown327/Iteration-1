254f351640941f4220ef0a75bc1074ddb098e7ca
Conor <brow4339@umn.edu>
2021-03-11 17:27:27 -0600

    Pusging package implementation

--- project/include/package.h
+++ project/include/package.h
@@ -1,14 +1,30 @@
-#include "entity_base.h"
+#ifndef PACKAGE_H
+#define PACKAGE_H
+
+#include "customer.h"
+//#include "vector3d.h"
 #include <vector>
 #include <string>
 
 namespace csci3081 {
 
-/*class Package : public csci3081::EntityBase {
+class Package{
  public:
     Package();
 
+    void SetCustomer(Customer *customer);
+    Customer GetCustomer();
+    void SetLocation(Vector3d *location);
+    Vector3d GetLocation();
+    void SetWeight(float weight);
+    float GetWeight();
+
   private:
-};*/
+    Customer *customer;
+    Vector3d *location;
+    float weight;
+};
 
 }
+
+#endif  // PACKAGE_H
--- project/src/package.cc
+++ project/src/package.cc
@@ -2,4 +2,17 @@
 
 namespace csci3081 {
 
+Package::Package()
+{
+    customer = new Customer();
+    location = new Vector3d();
+    weight = 0.0;
+}
+
+void Package::SetCustomer(Customer *customer) {this->customer = customer;}
+Customer Package::GetCustomer() {return *customer;}
+void Package::SetLocation(Vector3d *location) {this->location = location;}
+Vector3d Package::GetLocation() {return *location;}
+void Package::SetWeight(float weight) {this->weight = weight;}
+float Package::GetWeight() {return weight;}
 }
