9df66607f9dca1b5dac787c8a8d40adea8dafbf1
Conor <brow4339@umn.edu>
2021-03-11 17:12:53 -0600

    Pushing customer class implementation

--- project/src/customer.cc
+++ project/src/customer.cc
@@ -1,5 +1,11 @@
 #include "customer.h"
 
 namespace csci3081 {
+Customer::Customer()
+{
+    location = new Vector3d();
+}
 
+void Customer::SetLocation(Vector3d *location) {this->location = location;}
+Vector3d Customer::GetLocation() {return *location;}
 }
